046f11c2b2d2591b14e188aeb374dc5bdaf13f0a
Ba Faqas Ali <ali.ba-faqas@etudiant.univ-rennes.fr>
2024-04-15 22:04:08 +0200

    removing videoPlayer.mediaKeys

--- player.js
+++ player.js
@@ -101,10 +101,9 @@ window.onload = function() {
 
   let mpdUrl = 'https://storage.googleapis.com/wvmedia/cenc/h264/tears/tears.mpd';
 
-  if (!videoPlayer.mediaKeys) {
+  
     // Initializing the EME system on click
     initEME();
-    }
     player.load(mpdUrl).then(() => {
       console.log('La MPD a été chargée avec succès.');
     }).catch((error) => {
