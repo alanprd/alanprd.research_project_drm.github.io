2431066bb533b1b2b77b2d24cfdb08c6d315087a
Bassirou Badiane <66145208+Bachir-18@users.noreply.github.com>
2024-04-16 16:12:07 +0200

    Update player.js

--- player.js
+++ player.js
@@ -106,7 +106,7 @@ window.onload = function() {
   let mpdUrl = 'https://storage.googleapis.com/wvmedia/cenc/h264/tears/tears.mpd';
   
     // Initializing the EME system on click
-    //initEME();
+    initEME();
     videoPlayer.load(mpdUrl).then(() => {
       console.log('La MPD a été chargée avec succès.');
     }).catch((error) => {
